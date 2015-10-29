8e424abf2a4fecfcd0eb1858510207d6b7130e86
Simran Khosla <email.simrankhosla@gmail.com>
2015-10-29 16:23:09 -0500

    added user input

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,15 @@
-// hey hey hey
 
-
-//V SUPER IMPORTANT! It makes stuff happen only after everything on the page has loaded!
+//SUPER IMPORTANT! It makes stuff happen only after everything on the page has loaded!
 window.onload = function() {
-  (statusReport('hey hey hey!');
+  statusReport('hey hey hey!');
+
+  var username = prompt('What is your name?');
+  var mission = prompt('What is your mission?');
+
+  var contentContainer =
+  document.getElementById('content');
+
+  contentContainer.innerHTML = username + ' is on a mission to ' + mission;
 }
 
 function statusReport(message) {
