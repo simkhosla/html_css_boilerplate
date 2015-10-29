c35737e60ae2abbc52f84af195833b055e392a3b
Simran Khosla <email.simrankhosla@gmail.com>
2015-10-29 16:08:03 -0500

    updated some .js stuff

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+// hey hey hey
+
+
+//V SUPER IMPORTANT! It makes stuff happen only after everything on the page has loaded!
+window.onload = function() {
+  (statusReport('hey hey hey!');
+}
+
+function statusReport(message) {
+  console.log(message);
+  return message;
+}
